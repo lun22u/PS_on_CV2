98787ac6818ca9500f94afb0d0b634b32f479788
lun22u <73229629+lun22u@users.noreply.github.com>
2022-12-02 11:02:02 +0800

    Add files via upload

--- P S.py
+++ P S.py
@@ -7,64 +7,43 @@ class photo(object):
 
     def __init__(self, path):
         self.img = cv2.imread(path)
+        self.img_now = self.img
+        self.history = {}
 
     def lighten(self, a, b, c):
-        img0 = self.img
-        if not (a == b == c == d == 0):
+        img0 = self.img_now
+        infmg_1 = np.zeros(self.img.shape, np.uint8)
+        infmg_2 = np.zeros(self.img.shape, np.uint8)
+        infmg_3 = np.zeros(self.img.shape, np.uint8)
+        infmg_4 = np.zeros(self.img.shape, np.uint8)
+        if not (a == b == c == 0):
             cv2.bilateralFilter(img0, a, b, c, infmg_1)
             dst1 = infmg_1 - img0 + 128
             infmg_2 = cv2.GaussianBlur(dst1, (1, 1), 0, 0)
             infmg_3 = img0 + 2 * infmg_2 - 255
             infmg_4 = cv2.addWeighted(img0, 0.2, infmg_3, 0.8, 0)
-            img0 = cv2.add(img, infmg_4)  # img是原图，infmg_4是最终滤波的结果
+            img0 = cv2.add(self.img_now, infmg_4)  # img是原图，infmg_4是最终滤波的结果
         return img0
 
     def whiten(self, a):
-        img = self.img
+        img = self.img_now
         blur_img = cv2.bilateralFilter(img, 31, 75, 75)
         # 图像融合
-        result_img = cv2.addWeighted(img, a, blur_img, 1-a, 0)
-        cv2.imwrite("58_1.jpg", result_img)
-
-        image = Image.open("58_1.jpg")
+        # result_img = cv2.addWeighted(img, a, blur_img, 1-a, 0)
+        # cv2.imwrite("58_1.jpg", result_img)
+        img_1 = cv2.addWeighted(img, a, blur_img, 1-a, 0)
+        # image = Image.open("58_1.jpg")
         # 锐度调节
-        enh_img = ImageEnhance.Sharpness(image)
+        enh_img = ImageEnhance.Sharpness(img_1)
         image_sharped = enh_img.enhance(1.5)
         # 对比度调节
         con_img = ImageEnhance.Contrast(image_sharped)
         image_con = con_img.enhance(1.15)
-        image_con.save("58_2.jpg")
+        return image_con
 
-        img1 = cv2.imread("58.jpg")
-        img2 = cv2.imread("58_2.jpg")
-        cv2.imshow("1", img1)
-        cv2.imshow("2", img2)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
-
-        img = cv2.imread(fileName)
-        blur_img = cv2.bilateralFilter(img, 31, 75, 75)
-        # 图像融合
-        result_img = cv2.addWeighted(img, 0.3, blur_img, 0.7, 0)
-        cv2.imwrite("58_1.jpg", result_img)
-
-        image = Image.open("58_1.jpg")
-        # 锐度调节
-        enh_img = ImageEnhance.Sharpness(image)
-        image_sharped = enh_img.enhance(1.5)
-        # 对比度调节
-        con_img = ImageEnhance.Contrast(image_sharped)
-        image_con = con_img.enhance(1.15)
-        image_con.save("58_2.jpg")
-
-        img1 = cv2.imread("58.jpg")
-        img2 = cv2.imread("58_2.jpg")
-        cv2.imshow("1", img1)
-        cv2.imshow("2", img2)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
 
     def createWindow(self):
+        his = 1
         def noting(x):
             pass
         cv2.namedWindow('image')
@@ -76,3 +55,21 @@ class photo(object):
         b = cv2.getTrackbarPos('sigmaColor', 'image')
         c = cv2.getTrackbarPos('sigmaSpace', 'image')
         d = cv2.getTrackbarPos('getWhite', 'image')
+        while 1:
+            a2 = cv2.getTrackbarPos('d', 'image')
+            b2 = cv2.getTrackbarPos('sigmaColor', 'image')
+            c2 = cv2.getTrackbarPos('sigmaSpace', 'image')
+            d2 = cv2.getTrackbarPos('getWhite', 'image')
+            if a2 != a or b2 != b or c2 != c or d2 != d:
+                if a2 != a or b2 != b or c2 != c:
+                    self.img_now = self.lighten(a2, b2, c2)
+                if d2 != d:
+                    self.img_now = self.whiten(d2)
+                self.history[his] = [a2, b2, c2, d2]
+                a = a2, b = b2, c = c2, d = d2
+            cv2.imshow('ori', self.img)
+            cv2.imwrite("res.jpg", self.img_now)
+
+
+test = photo('test.png')
+test.createWindow()
