45052d9d947f87c094d5923ec9a2e1aff40f1364
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2022-12-05 18:20:46 +0800

    refactor: speed up func Face

--- foreend.py
+++ foreend.py
@@ -42,17 +42,12 @@ def White(img, value):
     return img
 
 
-
 def Skin(img_, a):
     if a == 0:
         return img_
     img = img_
     blur_img = cv2.bilateralFilter(img, 31, a, a)
-    # 图像融合
-    # result_img = cv2.addWeighted(img, a, blur_img, 1-a, 0)
-    # cv2.imwrite("58_1.jpg", result_img)
     img_1 = cv2.addWeighted(img, 0.5, blur_img, 0.5, 0)
-    # image = Image.open("58_1.jpg")
     # 锐度调节
     img_mid = Image.fromarray(cv2.cvtColor(img_1, cv2.COLOR_BGR2RGB))
     enh_img = ImageEnhance.Sharpness(img_mid)
@@ -66,40 +61,26 @@ def Skin(img_, a):
 
 
 predictor_path = './shape_predictor_68_face_landmarks.dat'
-
-# 使用dlib自带的frontal_face_detector作为我们的特征提取器
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(predictor_path)
 
+def landmark_dec_dlib_fun_for(rects, img_gray):
+    land_marks = []
+    for i in range(len(rects)):
+        land_marks_node = np.matrix([[p.x, p.y] for p in predictor(img_gray, rects[i]).parts()])
+        land_marks.append(land_marks_node)
+    return land_marks
 
 def landmark_dec_dlib_fun(img_src):
     img_gray = cv2.cvtColor(img_src, cv2.COLOR_BGR2GRAY)
-
-    land_marks = []
-
     rects = detector(img_gray, 0)
-
-    for i in range(len(rects)):
-        land_marks_node = np.matrix([[p.x, p.y] for p in predictor(img_gray, rects[i]).parts()])
-        # for idx,point in enumerate(land_marks_node):
-        #     # 68点坐标
-        #     pos = (point[0,0],point[0,1])
-        #     print(idx,pos)
-        #     # 利用cv2.circle给每个特征点画一个圈，共68个
-        #     cv2.circle(img_src, pos, 5, color=(0, 255, 0))
-        #     # 利用cv2.putText输出1-68
-        #     font = cv2.FONT_HERSHEY_SIMPLEX
-        #     cv2.putText(img_src, str(idx + 1), pos, font, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
-        land_marks.append(land_marks_node)
-
-    return land_marks
+    return landmark_dec_dlib_fun_for(rects, img_gray)
 
 
 # Interactive Image Warping 局部平移算法
-
+@jit
 def localTranslationWarp(srcImg, startX, startY, endX, endY, radius):
     ddradius = float(radius * radius)
-    copyImg = np.zeros(srcImg.shape, np.uint8)
     copyImg = srcImg.copy()
 
     # 计算公式中的|m-c|^2
@@ -133,6 +114,7 @@ def localTranslationWarp(srcImg, startX, startY, endX, endY, radius):
 
 
 # 双线性插值法
+@jit
 def BilinearInsert(src, ux, uy):
     w, h, c = src.shape
     if c == 3:
@@ -141,28 +123,17 @@ def BilinearInsert(src, ux, uy):
         y1 = int(uy)
         y2 = y1 + 1
 
-        part1 = src[y1, x1].astype(np.float) * (float(x2) - ux) * (float(y2) - uy)
-        part2 = src[y1, x2].astype(np.float) * (ux - float(x1)) * (float(y2) - uy)
-        part3 = src[y2, x1].astype(np.float) * (float(x2) - ux) * (uy - float(y1))
-        part4 = src[y2, x2].astype(np.float) * (ux - float(x1)) * (uy - float(y1))
+        part1 = src[y1, x1] * (float(x2) - ux) * (float(y2) - uy)
+        part2 = src[y1, x2] * (ux - float(x1)) * (float(y2) - uy)
+        part3 = src[y2, x1] * (float(x2) - ux) * (uy - float(y1))
+        part4 = src[y2, x2] * (ux - float(x1)) * (uy - float(y1))
 
         insertValue = part1 + part2 + part3 + part4
 
         return insertValue.astype(np.int8)
 
-
-# 瘦脸
-def Face(_img, face):
-    if face == 0:
-        return _img
-    face = face / 80
-    landmarks = landmark_dec_dlib_fun(_img)
-
+def Face_for(_img, landmarks, face):
     thin_image = _img
-    # 如果未检测到人脸关键点，就不进行瘦脸
-    if len(landmarks) == 0:
-        return _img
-
     for landmarks_node in landmarks:
         left_landmark = landmarks_node[3]
         left_landmark_down = landmarks_node[5]
@@ -186,12 +157,26 @@ def Face(_img, face):
         r_right *= face
 
         # 瘦左边脸
-        thin_image = localTranslationWarp(_img, left_landmark[0, 0], left_landmark[0, 1], endPt[0, 0], endPt[0, 1],
+        thin_image = localTranslationWarp(thin_image, left_landmark[0, 0], left_landmark[0, 1], endPt[0, 0], endPt[0, 1],
                                           r_left)
         # 瘦右边脸
         thin_image = localTranslationWarp(thin_image, right_landmark[0, 0], right_landmark[0, 1], endPt[0, 0],
                                           endPt[0, 1], r_right)
+
     return thin_image
+
+
+# 瘦脸
+def Face(_img, face):
+    if face == 0:
+        return _img
+    face = face / 80
+    landmarks = landmark_dec_dlib_fun(_img)
+    # 如果未检测到人脸关键点，就不进行瘦脸
+    if len(landmarks) == 0:
+        return _img
+
+    return Face_for(_img, landmarks, face)
 
 
 def process(src):
@@ -229,7 +214,7 @@ def process(src):
     dst = Skin(dst, skin)
 
     # 修脸  use face
-    #  dst = Face(dst, face)
+    dst = Face(dst, face)
 
     return dst
 
